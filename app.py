8fa885307483729d8e62c3e56312d9c36467dff2
caga5719 <caga5719@colorado.edu>
2024-04-06 00:32:41 +0000

    Established caga5719_db route to connect to db

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask
+import psycopg2
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World from Carlos Garcia in 3308!'
+
+@app.route('/caga5719_db')
+def caga5719_db():
+    conn = psycopg2.connect("caga5719_db")
+    conn.close()
+    return "Database Connection Successful"
+    
