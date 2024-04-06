f5ac04300b0a92bbeb713f4a874ba0be5825b4cb
caga5719 <caga5719@colorado.edu>
2024-04-06 01:49:30 +0000

    updated "db_test" as name for test route

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World from Carlos Garcia in 3308!'
 
-@app.route('/caga5719_db')
+@app.route('/db_test')
 def caga5719_db():
     conn = psycopg2.connect("postgres://caga5719_db_user:jgk8px6kecbxYAT0FrhSwflb43dSlugL@dpg-co89c5tjm4es738vrjj0-a/caga5719_db")
     conn.close()
