59c20cf62a38e4063f3a5add4d0d9fe1ec412346
caga5719 <caga5719@colorado.edu>
2024-04-06 00:48:34 +0000

    added db_create route to create Basketball table

--- app.py
+++ app.py
@@ -12,3 +12,19 @@ def caga5719_db():
     conn = psycopg2.connect("postgres://caga5719_db_user:jgk8px6kecbxYAT0FrhSwflb43dSlugL@dpg-co89c5tjm4es738vrjj0-a/caga5719_db")
     conn.close()
     return "Database Connection Successful"
+
+@app.route('db_create')
+def create_db():
+    conn = psycopg2.connect("postgres://caga5719_db_user:jgk8px6kecbxYAT0FrhSwflb43dSlugL@dpg-co89c5tjm4es738vrjj0-a/caga5719_db")
+    cur = conn.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int,
+        );
+    ''')
+    conn.commit()
+    conn.close()
+    return "Basketabll Table Successfuly created"
